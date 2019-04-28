a471d8b33046a35bcb142adda141cbb262764f8a
조성국 <jsk00123@naver.com>
2019-04-29 00:27:47 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/ktweb/KtwebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtwebApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtwebApplication>(*args)
+}
